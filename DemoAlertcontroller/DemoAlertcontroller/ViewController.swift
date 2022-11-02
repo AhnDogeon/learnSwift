f8689993e3eecd889ae06b98a3babbab61c95637
ahndogeon <qltiqlti@gmail.com>
2022-11-02 16:49:39 +0900

    221102 | alert swift2

--- DemoAlertcontroller/DemoAlertcontroller/ViewController.swift
+++ DemoAlertcontroller/DemoAlertcontroller/ViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func didAlert(_ sender: Any) {
-        let alert = UIAlertController(
+        let alert = UIAlertController(title:"알림", message: "중앙에 문자열",
+                                      preferredStyle: .alert)
+        // 버튼
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: {
+            (_) in
+            print("취소를 선택")
+        })
+        
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true)
     }
     
     
