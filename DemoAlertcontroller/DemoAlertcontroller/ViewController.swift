576332245600e523ec504728901fa6ab7a039c25
ahndogeon <qltiqlti@gmail.com>
2022-11-02 16:47:29 +0900

    221102 | alert swift

---
+++ DemoAlertcontroller/DemoAlertcontroller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DemoAlertcontroller
+//
+//  Created by 뚱건 on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func didAlert(_ sender: Any) {
+        let alert = UIAlertController(
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
