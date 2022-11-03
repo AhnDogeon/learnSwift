77a1c882224a621249879080f12ff50ebdb1a5df
ahndogeon <qltiqlti@gmail.com>
2022-11-03 11:37:10 +0900

    221103 | learn swift am

--- DemoAlertcontroller/DemoAlertcontroller/ViewController.swift
+++ DemoAlertcontroller/DemoAlertcontroller/ViewController.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func didAlert2(_ sender: Any) {
+        let alert = UIAlertController(title: "액션시트", message: "표준으로 사용하는 액션시트", preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler:nil)
+        let saveAction = UIAlertAction(title: "저장", style: .destructive, handler :
+                                        {
+            (_) in print("저장 작업을 합니다.")
+        })
+        
+        // 컨트롤러에 버튼 추가
+        alert.addAction(cancelAction)
+        alert.addAction(saveAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
     @IBAction func didAlert(_ sender: Any) {
         let alert = UIAlertController(title:"알림", message: "중앙에 문자열",
                                       preferredStyle: .alert)
